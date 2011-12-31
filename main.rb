d0996e2fd46298ad4c517177dc525af2289acd50
Werner Echezuria <w.echezuria@360releases.com>
2011-12-31 10:32:38 -0430

    jquery booklet added and working

--- main.rb
+++ main.rb
@@ -1,18 +1,18 @@
 require 'sinatra'
 require 'haml'
+require 'coffee-script'
+require './lib/partials'
+
+helpers Sinatra::Partials
 
 get '/' do
   haml :index
 end
 
-get '/skills' do
-  haml :skills
+get '/app.js' do
+  coffee :app
 end
 
-get '/work' do
-  haml :work
-end
-
-get '/contact' do
-  haml :contact
+get '/links.js' do
+  coffee :links
 end
---
+++ views/links.coffee
@@ -0,0 +1,11 @@
+$("#home").click ->
+  $("#mybook").booklet 1
+
+$("#skills").click ->
+  $("#mybook").booklet 2
+
+$("#work").click ->
+  $("#mybook").booklet 3
+
+$("#contact").click ->
+  $("#mybook").booklet 4
