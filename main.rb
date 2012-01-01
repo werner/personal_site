af501c75935bafcbc0e8cdb2f42ca347b3b2a19c
Werner Echezuria <w.echezuria@360releases.com>
2012-01-01 12:35:59 -0430

    separated routes code from the class

--- main.rb
+++ main.rb
@@ -5,36 +5,35 @@ require './lib/partials'
 
 class Main < Sinatra::Base
   register Barista::Integration::Sinatra
-  helpers Sinatra::Partials
-  
-  set :views, 'app/views'
-  set :port, 58031
-  
-  get '/' do
-    haml :index
-  end
-  
-  post '/contact' do
-    require 'pony'
-    Pony.options = {
-      :via => :smtp,
-      :via_options => {
-        :address => 'smtp.sendgrid.net',
-        :port => '587',
-        :domain => 'heroku.com',
-        :user_name => ENV['SENDGRID_USERNAME'],
-        :password => ENV['SENDGRID_PASSWORD'],
-        :authentication => :plain,
-        :enable_starttls_auto => true
-      }
+end
+
+helpers Sinatra::Partials
+
+set :views, 'app/views'
+
+get '/' do
+  haml :index
+end
+
+post '/contact' do
+  require 'pony'
+  Pony.options = {
+    :via => :smtp,
+    :via_options => {
+      :address => 'smtp.sendgrid.net',
+      :port => '587',
+      :domain => 'heroku.com',
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
     }
-   
-    Pony.mail :to => 'werner_a_e@yahoo.es',
-              :from => params[:message][:mail],
-              :subject => 'Name: ' + params[:message][:name] + ', send it from the personal site ',
-              :body =>  params[:message][:body]
-  end
+  }
+ 
+  Pony.mail :to => 'werner_a_e@yahoo.es',
+            :from => params[:message][:mail],
+            :subject => 'Name: ' + params[:message][:name] + ', send it from the personal site ',
+            :body =>  params[:message][:body]
 end
 
 Barista.compile_all!
-Main.run!
